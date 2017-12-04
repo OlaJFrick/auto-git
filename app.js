736679c10c00f2b6eab1eb9b6c7edf16c05c1d92
Ola Frick <info@culturehero.org>
2017-12-04 10:55:57 +0100

    Added pull functionality

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const simpleGit = require("simple-git/promise");
 
-// Add the Path to a repo
-'/Users/olafrick_/Desktop/reverse-proxy'
+async function pull(repoPath, branch) {
+  const repo = simpleGit(repoPath);
+  let status = await repo.pull("origin", "master");
+  console.log(status);
+}
+
+// Add the Path to a repo you have locally
+pull("/Users/olafrick_/Desktop/reverse-proxy");
