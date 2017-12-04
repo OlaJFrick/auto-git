32913ff7f043038a0e5b8a1b944ac06ec45161f7
Ola Frick <info@culturehero.org>
2017-12-04 11:39:06 +0100

    Added catch err

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const simpleGit = require("simple-git/promise");
+const path = require("path");
+const basePath = "/var/www";
+const repoInfo = require("./repos-and-branches.json");
 
+// Checkout a branch and make a pull
 async function pull(repoPath, branch) {
   const repo = simpleGit(repoPath);
-  let status = await repo.pull("origin", "master");
-  console.log(status);
+  let status = await repo.pull("origin", branch).catch(e => (err = e));
+
+  if (err) {
+    console.log("pull-error", err);
+  }
 }
 
-// Add the Path to a repo you have locally
-pull("/Users/olafrick_/Desktop/reverse-proxy");
+// Intervall pull all repos from github every 10 sec
+setInterval(() => {
+  for (let repo of repoInfo) {
+    pull(path.join(basePath, repo.path), repo.branch);
+  }
+}, 10000);
